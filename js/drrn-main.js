0b85320b78f578081d411aa3c952d47e64ffad69
darrinsnider <darrinsnider@users.noreply.github.com>
2015-07-16 00:13:29 -0400

    DRRN test js

---
+++ js/drrn-main.js
@@ -0,0 +1,43 @@
+/*jslint browser: true*/
+/*global $*/
+$(function () {
+    
+//smooth scrooling
+    'use strict';
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 300, 'swing');
+                //hide nav after scroll
+                setTimeout(function () {
+                    $('#nav').removeClass('nav-down').addClass('nav-up');
+                }, 330);
+                return false;
+            }
+        }
+    });
+    
+//hide nav on scroll
+    var lastScrollTop = 0, delta = 5;
+    $(window).scroll(function (event) {
+        var st = $(this).scrollTop();
+       
+        if (Math.abs(lastScrollTop - st) <= delta) {
+            return;
+        }
+        
+        if (st > lastScrollTop) {
+            // scroll down
+            $("#nav").removeClass('nav-down').addClass('nav-up');
+        } else {
+            // scroll up
+            $("#nav").removeClass('nav-up').addClass('nav-down');
+        }
+        lastScrollTop = st;
+    });
+});
