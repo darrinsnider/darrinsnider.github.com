57d950d9bc36b35bfbb2256312eba50e034e6552
darrinsnider <darrinsnider@users.noreply.github.com>
2015-07-16 22:11:06 -0400

    Updated nav for safari

--- js/drrn-main.js
+++ js/drrn-main.js
@@ -22,27 +22,49 @@ $(function () {
         }
     });
     
-//hide nav on scroll
-    var lastScrollTop = 0, delta = 5;
+// hide nav on scroll
+    var lastScrollTop = 0,
+        delta = 1;   // pixel move needed to trigger
     $(window).scroll(function (event) {
         var st = $(this).scrollTop(),
             scroll = $(window).scrollTop();
-       
-        if (Math.abs(lastScrollTop - st) <= delta) {
-            return;
-        }
         
-        if (st > lastScrollTop) {
-            // scroll down
-            $("#nav").removeClass('nav-down').addClass('nav-up');
+        if (scroll < 1) {
+            $("#nav").addClass('nav-top');
         } else {
-            // scroll up
-            $("#nav").removeClass('nav-up').addClass('nav-down');
-        }
-        lastScrollTop = st;
-        
-        if (scroll < 10) {
-            $("#nav").removeClass('nav-up').addClass('nav-down');
+            $("#nav").removeClass('nav-top');
+            if (st > lastScrollTop) {   // scroll down
+                $("#nav").addClass('nav-bye-bye');
+            } else if (st < lastScrollTop) {   // scroll up
+                $("#nav").removeClass('nav-bye-bye');
+            }
+            lastScrollTop = st;
         }
     });
+        
+        
+        
+       
+//    var lastScrollTop = 0, delta = 5;
+//    $(window).scroll(function (event) {
+//        var st = $(this).scrollTop(),
+//            scroll = $(window).scrollTop();
+//       
+//        if (Math.abs(lastScrollTop - st) <= delta) {
+//            return;
+//        }
+//        
+//        if (st > lastScrollTop) {
+//            // scroll down
+//            $("#nav").removeClass('nav-down').addClass('nav-up');
+//        } else {
+//            // scroll up
+//            $("#nav").removeClass('nav-up').addClass('nav-down');
+//        }
+//        lastScrollTop = st;
+//        
+//        if (scroll < 10) {
+//            $("#nav").removeClass('nav-up').addClass('nav-down');
+//        }
+//    });
 });
