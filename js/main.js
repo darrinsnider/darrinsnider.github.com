5974e6d00c3f1260d20c9fcb018e3f7a6f5488d2
darrin <drrrrnsnider@gmail.com>
2019-10-29 13:26:16 -0700

    zenscroll

--- js/main.js
+++ js/main.js
@@ -27,15 +27,14 @@ $(function () {
   });
 
   // native smooth scrolling
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
-
+//  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      //    anchor.addEventListener('click', function (e) {
+      //      e.preventDefault();
+      //      document.querySelector(this.getAttribute('href')).scrollIntoView({
+      //        behavior: 'smooth'
+      //      });
+      //    });
+      //  });
 
   //// click to play/pause
   //    $('.vid-click').click(function () {
